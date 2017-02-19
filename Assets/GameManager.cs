b996109b3c8bf791eca9f83e062bd1bfd1604180
Nermostrea <jaravyt@gmail.com>
2017-02-19 02:12:36 +0100

    Pause and Replay functionality added

---
+++ Assets/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool recording = true;
+
+    private float fixedTimestep;
+    private bool isPaused = false;
+
+	// Use this for initialization
+	void Start () {
+        fixedTimestep = Time.fixedDeltaTime;
+
+        PlayerPrefsManager.UnlockLevel(2);
+        Debug.Log("Unlock lvl 1 " + PlayerPrefsManager.IsLevelUnlocked(1));
+        Debug.Log("Unlock lvl 2 " + PlayerPrefsManager.IsLevelUnlocked(2));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (CrossPlatformInputManager.GetButton ("Fire1")) {
+            recording = false;
+        } else {
+            recording = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && isPaused) {
+            isPaused = false;
+            Resume();
+        } else if (Input.GetKeyDown(KeyCode.P) && !isPaused) {
+            isPaused = true;
+            Pause();
+        }
+    }
+
+    void Pause () {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+    }
+
+    void Resume() {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = fixedTimestep;
+    }
+
+    void OnApplicationPause (bool pauseStatus) {
+        isPaused = pauseStatus;
+    }
+}
---
+++ Assets/RePlay.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RePlay : MonoBehaviour {
+
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidBody;
+    private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (gameManager.recording) {
+            Record();
+        } else {
+            Playback();
+        }
+
+	}
+
+    void Playback () {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+//        Debug.Log("Playing frame: " + frame);
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    void Record () {
+        rigidBody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+//        Debug.Log("Frame number: " + frame);
+
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+// Structure for storing time, position and rotation
+public struct MyKeyFrame {
+
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation) {
+        frameTime = aTime;
+        position = aPosition;
+        rotation = aRotation;
+    }
+
+}
